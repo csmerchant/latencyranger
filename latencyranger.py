dadc5f07b884a6d06d4838c4b77fe1e7eff1bc43
aidensala <aidenmanansala1@gmail.com>
2018-12-16 22:59:18 -0800

    socket vars

--- latencyranger.py
+++ latencyranger.py
@@ -5,3 +5,6 @@ from threading import Thread
 #defining important variables for connections to the gameservers & other sockets
 gamesocket = None #we get the game socket once we connect to the gameserver
 sockhost = '0.0.0.0' #0.0.0.0 by default
+sockport = 3074 #port 3074 by default, change to whatever port you'd like
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #tcp socket
+tcpconn = tcpsock.connect((sockhost, sockport)) #tcp socket connector
